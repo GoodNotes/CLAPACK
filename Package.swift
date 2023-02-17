cf830b705248efbd0a2fee56d3f42fb15f245745
Antonio Candinho <antonio@goodnotesapp.com>
2023-02-17 08:50:34 +0100

    Compile f2clibs and blas

--- Package.swift
+++ Package.swift
@@ -12,9 +12,33 @@ let package = Package(
     .target(
       name: "CLAPACK",
       path: ".",
-      exclude: ["SRC/VARIANTS", "SRC/CMakeLists.txt", "SRC/Makefile"],
-      sources: ["SRC"],
-      publicHeadersPath: "INCLUDE"
+      exclude: [
+        "SRC/VARIANTS",
+        "SRC/CMakeLists.txt",
+        "SRC/Makefile",
+        "F2CLIBS/CMakeLists.txt",
+        "F2CLIBS/libf2c/Makefile",
+        "F2CLIBS/libf2c/README",
+        "F2CLIBS/libf2c/Notice",
+        "F2CLIBS/libf2c/CMakeLists.txt",
+        "F2CLIBS/libf2c/comptry.bat",
+        "F2CLIBS/libf2c/scomptry.bat",
+        "F2CLIBS/libf2c/main.c",
+        "F2CLIBS/libf2c/mkfile.plan9",
+        "F2CLIBS/libf2c/math.hvc",
+        "F2CLIBS/libf2c/libf2c.sy",
+        "F2CLIBS/libf2c/libf2c.lbc",
+        "F2CLIBS/libf2c/f2ch.add",
+        "BLAS/TESTING",
+        "BLAS/SRC/CMakeLists.txt",
+        "BLAS/SRC/Makefile",
+      ],
+      sources: ["SRC", "F2CLIBS", "BLAS/SRC"],
+      publicHeadersPath: "INCLUDE",
+      cSettings: [
+        .headerSearchPath("./F2CLIBS/libf2c/"),
+        .unsafeFlags(["-DINTEGER_STAR_8"]),
+      ]
     )
   ]
 )
